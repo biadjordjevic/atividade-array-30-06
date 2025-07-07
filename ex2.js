21574a4f77f555c734c1364ad76b74626edb96b1
Aluno SENAI <Aluno@SN-370641.edu.fiesc.com.br>
2025-07-07 10:14:54 -0300

    corrigindo atvd

--- ex2.js
+++ ex2.js
@@ -1,5 +1,4 @@
-
-function separaParImpar(nums){
+function separaParImpar(numeros){
 
     let numeros = [1,2,3,4,5,6,7,8,9]
     let pares = []
@@ -7,4 +6,18 @@ function separaParImpar(nums){
     let iPar = 0
     let iImpar = 0
     
+    for (let i = 0; i<numeros.length; i++){
+        if (numeros[1] %2 === 0){
+            pares[iPar] = numeros[5]
+            iPar++
+        }
+        else{
+            impares[iImpar] = numeros[1]
+            iImpar++
+        }
+    }
+    console.log(iPar);
+    console.log(iImpares)
+    separaParImpar(numeros)
+
 }
